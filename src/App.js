85d660c125d2656a591550ddcfe4f92bb85cbb28
nuraga123 <92333033+nuraga123@users.noreply.github.com>
2022-01-10 03:17:24 +0400

    [добавил 3 картинки и редактирвал сделал через один элеиент не успел все]-[added 3 pictures and edited it through one element did not have time for everything]

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import FeaturesContent from "./FeaturesContent/FeaturesContent";
 import HeaderLagoona from "./HeaderLagoona/HeaderLagoona";
 import HeaderNavigation from "./HeaderNavigation/HeaderNavigation";
 
@@ -8,16 +9,7 @@ function App() {
     <div>
       <HeaderLagoona/>
       <HeaderNavigation/>
-      <div className="main">
-        <h1 className="title">Спецпредложения</h1>
-        <div className="main__contayner1">
-          <img className="main__img1" src="/imgLoga/main1.jpg"/>
-          <h2 className="img1__title">Мальдивские острова</h2>
-          <span className="img1__price">от 55 000 р</span>
-          <span className="details">Подробнее</span>
-          <div className="img1__strelka"></div>
-        </div>
-      </div>
+      <FeaturesContent/>
     </div>
   );
 };
---
+++ src/elements/elements.js
@@ -0,0 +1,23 @@
+const elements = [
+    {
+        title: "Мальдивские острова",
+        img: "/imgLoga/main1.jpg",
+        price: "от 55 000 р", 
+        details: "Подробнее"
+    },
+    {
+        title: "Горящий тур на остров Крит",
+        img: "/imgLoga/main2.png",
+        price: "от 30 000 р", 
+        details: "Подробнее"
+    },
+    // {
+    //     title: "Номера категории люкс",
+    //     img: "/imgLoga/main3.png",
+    //     price: "от 5000 р", 
+    //     details: "Подробнее"
+    // }
+]
+
+
+export default elements;
